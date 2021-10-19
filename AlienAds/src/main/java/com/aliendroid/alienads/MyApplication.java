1e10705de8da85defa7b16f5f451fa1a96c0afce
rysasahrial-99 <rysasahrial@gmail.com>
2021-10-19 19:45:56 +0700

    Edit Native Ads - Medium Ads

--- AlienAds/src/main/java/com/aliendroid/alienads/MyApplication.java
+++ AlienAds/src/main/java/com/aliendroid/alienads/MyApplication.java
@@ -1,7 +1,10 @@
 package com.aliendroid.alienads;
 
 import android.app.Application;
+import android.util.Log;
 
+import com.flurry.android.FlurryAgent;
+import com.flurry.android.FlurryPerformance;
 import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.initialization.InitializationStatus;
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
@@ -20,7 +23,13 @@ public class MyApplication extends Application {
                     }
                 });
 
-
+        new FlurryAgent.Builder()
+                .withDataSaleOptOut(false)
+                .withCaptureUncaughtExceptions(true)
+                .withIncludeBackgroundSessionsInMetrics(true)
+                .withLogLevel(Log.VERBOSE)
+                .withPerformanceMetrics(FlurryPerformance.ALL)
+                .build(this, "9CFC3HDGJYSFTRKJG98N");
         alienOpenAds = new AlienOpenAds(this);
 
 
