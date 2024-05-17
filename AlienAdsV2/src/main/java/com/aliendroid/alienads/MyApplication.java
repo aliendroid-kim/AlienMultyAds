decde9b028407e2ebcace9fef68d66168dbdaed4
reskin-aliendroid <rysasahrial@aliendro.id>
2024-05-17 10:14:57 +0700

    Update AppMetrica

--- AlienAdsV2/src/main/java/com/aliendroid/alienads/MyApplication.java
+++ AlienAdsV2/src/main/java/com/aliendroid/alienads/MyApplication.java
@@ -3,12 +3,17 @@ package com.aliendroid.alienads;
 import static com.aliendroid.sdkads.config.AppsConfig.ALIENSDKKEY;
 
 import android.annotation.SuppressLint;
+import android.app.ActivityManager;
 import android.app.Application;
 import android.content.Context;
+import android.os.Build;
+import android.webkit.WebView;
 
 import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.initialization.InitializationStatus;
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
+import java.util.List;
 
 import io.appmetrica.analytics.AppMetrica;
 import io.appmetrica.analytics.AppMetricaConfig;
@@ -24,29 +29,58 @@ public class MyApplication extends Application {
     private static ApplovinOpenAds applovinOpenAds;
     @SuppressLint("StaticFieldLeak")
     private static AlienNotif notif;
-    Context context;
+    static Context context;
     @Override
     public void onCreate() {
         super.onCreate();
         context = this;
-        MobileAds.initialize(
-                this,
-                new OnInitializationCompleteListener() {
-                    @Override
-                    public void onInitializationComplete(InitializationStatus initializationStatus) {
-                    }
-                });
-        AppMetricaConfig config = AppMetricaConfig
-                .newConfigBuilder(ALIENSDKKEY)
-                .withSessionTimeout(15)
-                .withCrashReporting(true)
-                .build();
-        AppMetrica.activate(this, config);
-        AppMetrica.enableActivityAutoTracking(this);
-        alienOpenAds = new AlienOpenAds(this);
-        propsOpenAds = new PropsOpenAds(this);
-        jsmboxOpenAds = new JamboxOpenAds(this);
-        notif = new AlienNotif(context,this);
 
+        if (isMainProcess()) {
+            alienOpenAds = new AlienOpenAds(this);
+            propsOpenAds = new PropsOpenAds(this);
+            jsmboxOpenAds = new JamboxOpenAds(this);
+            notif = new AlienNotif(context,this);
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                String processName = getProcessName();
+                try {
+                    WebView.setDataDirectorySuffix(processName);
+                } catch (Throwable e) {
+                    // ignore
+                }
+            }
+
+            MobileAds.initialize(
+                    this,
+                    new OnInitializationCompleteListener() {
+                        @Override
+                        public void onInitializationComplete(InitializationStatus initializationStatus) {
+                        }
+                    });
+            AppMetricaConfig config = AppMetricaConfig
+                    .newConfigBuilder(ALIENSDKKEY)
+                    .withSessionTimeout(15)
+                    .withCrashReporting(true)
+                    .build();
+            AppMetrica.activate(this, config);
+            AppMetrica.enableActivityAutoTracking(this);
+        }
+
+
+
+    }
+    private boolean isMainProcess() {
+        return getPackageName().equals(getProcessName());
+    }
+    public static String getProcessName() {
+        int mypid = android.os.Process.myPid();
+        ActivityManager manager = (ActivityManager)  context.getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningAppProcessInfo> infos = manager.getRunningAppProcesses();
+        for(ActivityManager.RunningAppProcessInfo info : infos) {
+            if (info.pid == mypid) {
+                return info.processName;
+            }
+        }
+        return null;
     }
 }
