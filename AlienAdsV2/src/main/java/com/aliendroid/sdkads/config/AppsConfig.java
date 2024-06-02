e00a3f186f4b1f0416c6a2b30fdb9e38e6212457
reskin-aliendroid <rysasahrial@aliendro.id>
2024-06-02 19:44:55 +0700

    sdk key for Games

--- AlienAdsV2/src/main/java/com/aliendroid/sdkads/config/AppsConfig.java
+++ AlienAdsV2/src/main/java/com/aliendroid/sdkads/config/AppsConfig.java
@@ -16,7 +16,8 @@ import com.aliendroid.alienads.AdjustHelper;
 import java.util.List;
 
 public class AppsConfig {
-    public static String ALIENSDKKEY = "uw277yqlu1hc";
+    public static String GAMEKEY ="T7PPns0K6JV00uGv0ZAEKsTWrpwA-N4Hchi_KKecaqTa_U5zQcyyoI_pTcC5TM1OgfrLz5dWGdASKWgK6l5Sks";
+    public static String ALIENSDKKEY = "bgxb4gvpki68";
     public static void TRACKING (Context context){
         AdjustHelper.Initialize(context, ALIENSDKKEY, AdjustHelper.AdjustEnv.ENVIRONMENT_PRODUCTION);
 
